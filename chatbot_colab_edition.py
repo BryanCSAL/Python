505b19c121d2e409d0accc2271b5a80b14b7920d
BCSAL <124710121+BryanCSAL@users.noreply.github.com>
2024-10-28 21:39:07 -0300

    Update chatbot_colab_edition.py
    Corrigindo erro de syntaxe.

--- chatbot_colab_edition.py
+++ chatbot_colab_edition.py
@@ -1,19 +1,19 @@
 import google.generativeai as genai
 from google.colab import userdata
 
-// Adicione uma chave de api do Gemini (https://aistudio.google.com/apikey)
+# Adicione uma chave de api do Gemini (https://aistudio.google.com/apikey)
 GOOGLE_GEMINI_API_KEY = userdata.get('GOOGLE_GEMINI_API_KEY')
 
-// Aplicando a key
+# Aplicando a key
 genai.configure(api_key=GOOGLE_GEMINI_API_KEY)
 
-// Escolhendo o modelo
+# Escolhendo o modelo
 model = genai.GenerativeModel("gemini-1.5-pro-latest")
 
-// Defininindo um lista que servirá de histórico, para que o chatbot se lembre da pergunta anterior
+# Defininindo um lista que servirá de histórico, para que o chatbot se lembre da pergunta anterior
 chat = model.start_chat(history=[])
 
-// Entrada/Saída e maneira de parar conversa com o chatbot
+# Entrada/Saída e maneira de parar conversa com o chatbot
 prompt = input("Esperando Prompt: ")
 while prompt != "sair":
   response = chat.send_message(prompt)
